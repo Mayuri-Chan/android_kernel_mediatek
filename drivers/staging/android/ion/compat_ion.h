57d0dee88846392b10bb48acf2d04fbb7934a734
wulan17 <wulan17@nusantararom.org>
2023-07-14 19:04:09 +0000

    drivers: staging: import t-alps-q0.mp1-V9.122.1 changes
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- drivers/staging/android/ion/compat_ion.h
+++ drivers/staging/android/ion/compat_ion.h
@@ -1,5 +1,7 @@
 /*
- * Copyright (C) 2014 Google, Inc.
+ * drivers/staging/android/ion/compat_ion.h
+ *
+ * Copyright (C) 2013 MediaTek, Inc.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -12,9 +14,16 @@
  *
  */
 
-#ifndef _FIQ_WATCHDOG_H_
-#define _FIQ_WATCHDOG_H_
+#ifndef _LINUX_COMPAT_ION_H
+#define _LINUX_COMPAT_ION_H
 
-void fiq_watchdog_triggered(const struct pt_regs *regs, void *svc_sp);
+#if IS_ENABLED(CONFIG_COMPAT)
 
-#endif
+long compat_ion_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+
+#else
+
+#define compat_ion_ioctl  NULL
+
+#endif /* CONFIG_COMPAT */
+#endif /* _LINUX_COMPAT_ION_H */
